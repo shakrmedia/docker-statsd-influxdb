e48c25e5447521eeddc64074434dabaa91ae7bf2
Minku Lee <premist@me.com>
2014-12-11 21:06:20 +0900

    Allow to set up InfluxDB host/port via alternative env var

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 {
   influxdb: {
-    host: process.env.INFLUXDB_HOST || "localhost",
-    port: process.env.INFLUXDB_PORT || 8086,
+    host: process.env.INFLUXDB_HOST || process.env.INFLUXDB_MASTER_SERVICE_HOST || "localhost",
+    port: process.env.INFLUXDB_PORT || process.env.INFLUXDB_MASTER_SERVICE_PORT || 8086,
     database: process.env.INFLUXDB_DATABASE || "site_dev",
     username: process.env.INFLUXDB_USERNAME || "root",
     password: process.env.INFLUXDB_PASSWORD || "root",
